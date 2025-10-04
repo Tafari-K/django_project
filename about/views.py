68f5d3b97a66e693cca6f080b0c4798faf14d214
Tafari-K <kirton.tafari@gmail.com>
2025-10-04 11:17:22 +0100

    Add: View method for data handling

--- about/views.py
+++ about/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+fom django.http import HttpResponse
 
 # Create your views here.
+def about_me(request):
+    return HttpResponse("This would be the about page" )
+def about_me(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something" )
+    return HttpResponse(request.method)
